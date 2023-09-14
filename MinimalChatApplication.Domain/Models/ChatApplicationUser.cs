41cb4c016d062ca14a33f49577d960a1de1c7843
Ashish8532 <ashishraj916263@outlook.com>
2023-09-14 17:59:17 +0530

    Implement database setup

---
+++ MinimalChatApplication.Domain/Models/ChatApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalChatApplication.Domain.Models
+{
+    public class ChatApplicationUser: IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
