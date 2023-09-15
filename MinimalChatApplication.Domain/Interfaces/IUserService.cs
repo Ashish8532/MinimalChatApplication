e538342c5f556a37af06e344ff41a15ee895d7f6
Ashish8532 <ashishraj916263@outlook.com>
2023-09-15 15:12:17 +0530

    Implement get all user api except logged in user

--- MinimalChatApplication.Domain/Interfaces/IUserService.cs
+++ MinimalChatApplication.Domain/Interfaces/IUserService.cs
@@ -53,5 +53,19 @@ namespace MinimalChatApplication.Domain.Interfaces
         /// <param name="user">The user for whom the token is generated.</param>
         /// <returns>The JWT token as a string.</returns>
         string GenerateJwtToken(ChatApplicationUser user);
+
+        /// <summary>
+        /// Retrieves a list of users excluding the current user or returns all users if currentUserId is null.
+        /// </summary>
+        /// <param name="currentUserId">The unique identifier of the current user. Pass null to retrieve all users.</param>
+        /// <returns>
+        /// A collection of ChatApplicationUser objects representing users, excluding the current user.
+        /// If currentUserId is null, it returns all users.
+        /// </returns>
+        /// <remarks>
+        /// This method queries the database to retrieve all users except the one identified by the provided currentUserId. 
+        /// If currentUserId is null, it returns all users available in the database.
+        /// </remarks>
+        Task<IEnumerable<ChatApplicationUser>> GetUsersExceptCurrentUserAsync(string currentUserId);
     }
 }
