8075f2482641be79bd049983baccda73f77fe39b
Ashish8532 <ashishraj916263@outlook.com>
2023-11-03 10:16:30 +0530

    Added the method description

--- MinimalChatApplication.Domain/Interfaces/IGifService.cs
+++ MinimalChatApplication.Domain/Interfaces/IGifService.cs
@@ -9,9 +9,18 @@ namespace MinimalChatApplication.Domain.Interfaces
 {
     public interface IGifService
     {
+        /// <summary>
+        /// Retrieves all GIF data asynchronously from the repository.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a collection of GIF data.</returns>
         Task<IEnumerable<GifData>> GetAllGifDataAsync();
 
 
+        /// <summary>
+        /// Adds GIF data with the provided name to the repository and saves the changes.
+        /// </summary>
+        /// <param name="gifName">The name of the GIF to add to the repository.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         Task AddGifDataAsync(string gifName);
     }
 }
